5c21c1e9fe38763cdf6f9f2ead912c3a691e4119
Philip Kim <156368225+toastexmachina@users.noreply.github.com>
2024-12-10 11:15:00 -0600

    Add function to store form values in object and display in table
    - Added function to event listener
    - Function will create a new Book object using the form values
    - The function pushes the object to the library array
    - A new row is added to the table and the form values are displayed
    as text
    - The form is then reset to empty strings (Needs to be changed so it
    restores to defaults instead)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const myLibrary = [];
 
 // Variable Declaration
 const form = document.querySelector('form');
+const tableLibrary = document.querySelector('table');
 
 // Book Constructor
 function Book(title, medium, status) {
@@ -9,7 +10,7 @@ function Book(title, medium, status) {
     this.title = title;
     this.medium = medium;
     this.status = status;
-};
+}
 
 function addBookToLibrary() {
     // moves Book objects into myLibrary Array
@@ -24,15 +25,29 @@ let buttonSubmit = document.querySelector('#submit');
 
 buttonSubmit.addEventListener('click', function(event){
     event.preventDefault();
+
+    // Create new table data elements
+    let newEntry = document.createElement('tr');
+    tableLibrary.appendChild(newEntry);
+    let newTitle = document.createElement('td');
+    let newMedium = document.createElement('td');
+    let newStatus = document.createElement('td');
+    newEntry.appendChild(newTitle);
+    newEntry.appendChild(newMedium);
+    newEntry.appendChild(newStatus);
+
     // call Book function here
-    let newBook = Book (form.title.value, form.medium.checked, form.status.checked);
+    let newBook = new Book(form.title.value, form.medium.value, form.status.value);
     // Push new object to the arry
     myLibrary.push(newBook);
+
+    // Add text content to the new table data
+    newTitle.textContent = form.title.value;
+    newMedium.textContent = form.medium.value;
+    newStatus.textContent = form.status.value;
+
     // Clear the Form
     form.title.value = '';
-    form.medium.checked = '';
-    form.status.checked ='';
-})
-// Array entry values get added to the table
-//
-
+    form.medium.value = '';
+    form.status.value ='';
+});
